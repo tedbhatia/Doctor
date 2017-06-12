92ba41dddd2681a7afc86df54fc7dd56cccab68b
tedbhatia <ted.bhatia@gmail.com>
2017-06-12 18:46:23 +0530

    my doctors added1

---
+++ app/src/main/java/com/example/doctor/ui/model/Doctor.java
@@ -0,0 +1,55 @@
+package com.example.doctor.ui.model;
+
+import java.io.Serializable;
+
+/**
+ * Created by tejas on 12/6/17.
+ */
+
+public class Doctor implements Serializable{
+    private String name;
+    private String type;
+    private String address;
+    private String phone;
+    private String notes;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+}
