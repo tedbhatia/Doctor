a58dc4b5eb31f391935a950a9d4f43ebc594714e
tedbhatia <ted.bhatia@gmail.com>
2017-06-20 06:22:17 +0530

    stuff added

--- app/src/main/java/com/example/doctor/support/firebase/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/doctor/support/firebase/MyFirebaseMessagingService.java
@@ -22,18 +22,30 @@ import android.content.Context;
 import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.renderscript.RenderScript;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
+import android.widget.Toast;
 
 
 import com.example.doctor.R;
+import com.example.doctor.support.service.MyJobService;
 import com.example.doctor.ui.activity.MainActivity;
+import com.firebase.jobdispatcher.FirebaseJobDispatcher;
+import com.firebase.jobdispatcher.GooglePlayDriver;
+import com.firebase.jobdispatcher.Job;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+
+import static android.R.id.message;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
     private static final String TAG = "MyFirebaseMsgService";
+    private Handler mHandler;
 
     /**
      * Called when message is received.
@@ -58,12 +70,15 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         Log.d(TAG, "From: " + remoteMessage.getFrom());
 
         // Check if message contains a data payload.
+
         if (remoteMessage.getData().size() > 0) {
             Log.d(TAG, "Message data payload: " + remoteMessage.getData());
 
             if (/* Check if data needs to be processed by long running job */ true) {
+                //Toast.makeText(this,remoteMessage.getData().get(0),Toast.LENGTH_SHORT).show();
                 // For long-running tasks (10 seconds or more) use Firebase Job Dispatcher.
                 //scheduleJob();
+                sendNotification(remoteMessage.getNotification().toString());
             } else {
                 // Handle message within 10 seconds
                 handleNow();
@@ -74,6 +89,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         // Check if message contains a notification payload.
         if (remoteMessage.getNotification() != null) {
             Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+            sendNotification(remoteMessage.getNotification().toString());
         }
 
         // Also if you intend on generating your own notifications as a result of a received FCM
@@ -93,8 +109,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
                 .build();
         dispatcher.schedule(myJob);
         // [END dispatch_job]
-    }
-*/
+    }*/
     /**
      * Handle time allotted to BroadcastReceivers.
      */
@@ -127,4 +142,6 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
         notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
     }
+
+
 }
